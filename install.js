e74da3590471ce0a926f07e006d14e0dc5846dce
Fritz Lin <uxfritz@163.com>
2016-09-30 15:37:50 +0800

    init

---
+++ install.js
@@ -0,0 +1,27 @@
+// Building for sqlcipher
+// https://github.com/mapbox/node-sqlite3#building-for-sqlcipher
+'use strict'
+require('shelljs/global')
+
+if (process.plarform === 'win32') {
+  // windows
+  throw new Error('`win32` is not supported. Perhaps you should look for `cross-sqlcipher`.')
+} else if (process.platform === 'darwin') {
+  // macos
+  if (exec('which brew').stdout.trim() === '') {
+    throw new Error('`brew` is required to be installed.')
+  }
+  if (exec('brew list sqlcipher').code !== 0) {
+    // exec('brew install sqlcipher')
+    exec('brew install sqlcipher --with-fts')
+  }
+  exec('export LDFLAGS="-L`brew --prefix`/opt/sqlcipher/lib"')
+  exec('export CPPFLAGS="-I`brew --prefix`/opt/sqlcipher/include"')
+  exec('npm install sqlite3 --build-from-source --sqlite_libname=sqlcipher --sqlite=`brew --prefix`')
+} else {
+  // linux
+  exec('export LDFLAGS="-L/usr/local/lib"')
+  exec('export CPPFLAGS="-I/usr/local/include -I/usr/local/include/sqlcipher"')
+  exec('export CXXFLAGS="$CPPFLAGS"')
+  exec('npm install sqlite3 --build-from-source --sqlite_libname=sqlcipher --sqlite=/usr/local --verbose')
+}

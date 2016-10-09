2bb825b0b767bbab5f55c79613695bab2cf23d13
Fritz Lin <uxfritz@163.com>
2016-10-09 11:39:48 +0800

    0.0.3 - pass npm_config_argv for target build, such as nwjs https://github.com/Gaubee/blog/issues/91 https://github.com/mapbox/node-sqlite3/issues/417 https://github.com/mapbox/node-sqlite3/issues/497 https://github.com/mapbox/node-sqlite3/issues/587

--- postinstall.js
+++ postinstall.js
@@ -2,6 +2,22 @@
 // https://github.com/mapbox/node-sqlite3#building-for-sqlcipher
 'use strict'
 require('shelljs/global')
+var isArray = require('util').isArray
+
+var args
+try {
+  args = JSON.parse(process.env.npm_config_argv).original
+} finally {
+  if (!isArray(args)) {
+    args = []
+  }
+}
+var targetArgs = args.filter(function (arg) {
+  return /^--(runtime|target)/.test(arg)
+})
+var targetStr = targetArgs.reduce(function (m, arg) {
+  return m + ' ' + arg
+}, '')
 
 // not windows
 if (process.platform === 'darwin') {
@@ -15,11 +31,11 @@ if (process.platform === 'darwin') {
   }
   exec('export LDFLAGS="-L`brew --prefix`/opt/sqlcipher/lib"')
   exec('export CPPFLAGS="-I`brew --prefix`/opt/sqlcipher/include"')
-  exec('npm install sqlite3 --build-from-source --sqlite_libname=sqlcipher --sqlite=`brew --prefix`')
+  exec('npm install sqlite3 --build-from-source --sqlite_libname=sqlcipher --sqlite=`brew --prefix`' + targetStr)
 } else {
   // linux
   exec('export LDFLAGS="-L/usr/local/lib"')
   exec('export CPPFLAGS="-I/usr/local/include -I/usr/local/include/sqlcipher"')
   exec('export CXXFLAGS="$CPPFLAGS"')
-  exec('npm install sqlite3 --build-from-source --sqlite_libname=sqlcipher --sqlite=/usr/local --verbose')
+  exec('npm install sqlite3 --build-from-source --sqlite_libname=sqlcipher --sqlite=/usr/local --verbose' + targetStr)
 }
